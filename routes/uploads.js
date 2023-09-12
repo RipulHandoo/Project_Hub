0e38165024f0cef83d3181d8fe0d8ef5f5b27d40
Ripul Handoo <ripulhandoo1234@gmail.com>
2023-09-12 16:31:29 +0530

    Changing Routes from Static to Dyanmic

--- routes/uploads.js
+++ routes/uploads.js
@@ -2,23 +2,29 @@ const express = require("express")
 const multer = require("multer")
 const authentication = require("../middleware/auth")
 const router = express.Router()
-const uuid = require("uuid")
 const PutObject = require("../s3Service/putObject")
-const listObject = require("../s3Service/listObject")
+const listObject = require("../s3Service/listUserObjects")
 const createRepo = require("../s3Service/createRepo")
+const ListObjects = require("../s3Service/listObjects")
+const getObject = require("../s3Service/getObject")
+const deleteObjectCommand = require("../s3Service/deleteObject")
 
 
 const storage = multer.memoryStorage()
 const uploads = multer({storage: storage, limits: {fileSize: 2000000}})
 
-router.post("/uploads", authentication, uploads.array("files", 2), async (req, res) => {
+router.post("/:username/:repository/uploads/", authentication, uploads.array("files", 2), async (req, res) => {
   const files = req.files; // Use req.files directly, it's an array
 
   try {
-    const result = await PutObject(files, req, res);
+    const username = req.params.username;
+    const repositoryName = req.params.repository;
+
+    const result = await PutObject(`${username}/${repositoryName}`,files, req, res);
     
     res.status(201).json({
       message: "Files uploaded successfully",
+      result: result
     });
   } catch (error) {
     console.error("Error uploading files:", error);
@@ -29,9 +35,10 @@ router.post("/uploads", authentication, uploads.array("files", 2), async (req, r
 });
 
 // this is the end point when the user goes to its repository
-router.get("/listObject",authentication, async(req,res) => {
+router.get("/:username/",authentication, async(req,res) => {
   try{
-    const response = await listObject(req,res);
+    const username = req.params.username;
+    const response = await listObject(req,res,`${username}`);
     // from here we will send the response to the front end
     res.status(200).json({
       message: "Files listed successfully",
@@ -57,6 +64,72 @@ router.get("/createRepo",authentication, async(req,res) => {
   }
   catch(error){
     console.error("Error creating repository:", error);
+  }
+});
+
+// Route to delete the file's or folder's in user's repo
+router.get("/:username/:repository/delete/*",authentication, async(req,res) => {
+  try{
+      // call the function to delete the object at the path
+      const username = req.params.username;
+      const repositoryName = req.params.repository;
+      const additionalLevels = req.params[0].split("/"); // Capture additional levels as an array
+
+      const response = await deleteObjectCommand(req,res,`${username}/${repositoryName}/${additionalLevels.join("/")}`);
+
+      res.status(200).json({
+        message:"Object deleted successfully",
+        response: response
+      });
+  }
+  catch(error){
+    console.error("Error deleting file:", error);
+    res.status(500).json({
+      message: error
+    });
+  }
+});
+
+// Route for user repositories with multiple levels
+router.get("/:username/:repository/*", authentication, async (req, res) => {
+  try {
+      const username = req.params.username;
+      const repositoryName = req.params.repository;
+      const additionalLevels = req.params[0].split("/"); // Capture additional levels as an array
+
+      // Get the last part of the URL (file or directory name)
+      const lastPart = additionalLevels[additionalLevels.length - 1];
+
+      // Check if the last part contains a period (dot) to determine if it's a file
+      const isFile = lastPart.includes(".");
+
+      if (isFile) {
+          // Handle the case where the last part is a file
+          // You can implement logic to serve the file or perform file-related operations.
+          const response = await getObject(req,res,`${username}/${repositoryName}/${additionalLevels.join("/")}`);
+
+          res.status(200).json({
+              message: "File retrieved successfully",
+              response: response,
+              username: username,
+              repository: repositoryName,
+              filename: lastPart,
+          });
+      } else {
+          // Handle the case where the last part is a directory
+          // You can implement logic to list directory contents or perform directory-related operations.
+         const response = await listObject(req,res,`${username}/${repositoryName}`);
+
+         res.status(200).json({
+          message: "Files listed successfully",
+          response: response
+        });
+      }
+  } catch (error) {
+      console.error("Error handling route:", error);
+      res.status(500).json({
+          error: "Internal server error",
+      });
   }
 });
 
